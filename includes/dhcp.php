90ddda0c78ac07e30afaf99d547c249fc535360b
Shawn Holland <seholland@gmail.com>
2017-04-23 16:10:58 -0400

    no message

--- includes/dhcp.php
+++ includes/dhcp.php
@@ -190,7 +190,7 @@ function DisplayDHCPConfig() {
             <tbody>
               <tr>
                 <?php
-                exec( '/usr/sbin/dhcp-lease-list --parsable --lease ' . RASPI_ISC_DHCP_LEASES . ' 2>&1', $leases, $return );
+                exec( 'sudo /usr/sbin/dhcp-lease-list --parsable --lease ' . RASPI_ISC_DHCP_LEASES . ' 2>&1', $leases, $return );
                 var_dump($leases);
                 var_dump($return);
                 foreach( $leases as $lease ) {
